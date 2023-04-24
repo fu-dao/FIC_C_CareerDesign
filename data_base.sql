e549c44af1cab939e5a01aebc1b9d671fbaef08c
Hirokishigyou <hiroki.s.23_03@i.softbank.jp>
2023-04-24 14:29:31 +0900

    Update data_base.sql

--- data_base.sql
+++ data_base.sql
@@ -5,11 +5,10 @@ grant all on shitamichi.* to 'staff'@'localhost' identifid by 'password';
 use shitamichi;
 
 -- テーブル名　hood_menu
--- 左からID、商品名、値段
 create table hood_menu(
-    id int auto_increment primary key,
-    name varchar(200) not null,
-    price int not null
+    id int auto_increment primary key,  --ID（null)
+    name varchar(200) not null,         --商品名（200文字まで）
+    price int not null                  --値段
 );
 -- 多分作り直したほうが良さそうなゾーン
 insert into hood_menu values(null, 'クッキー', 120);
@@ -21,14 +20,13 @@ insert into hood_menu values(null, 'パンケーキ', 500);
 insert into hood_menu values(null, 'フレンチトースト', 250);
 
 -- テーブル名　drink_menu
--- 左からID、商品名、M値段、S、L
 -- S価格はhtmlで「-」とかつけといてください
 create table drink_menu{
-    id int auto_increment primary key,
-    name varchar(200) not null,
-    price_M int not null,
-    price_S int not null,
-    price_L int not null
+    id int auto_increment primary key,  --ID（null）
+    name varchar(200) not null,         --商品名（200文字まで）
+    price_M int not null,               --M価格
+    price_S int not null,               --S価格（-xx）
+    price_L int not null                --L価格（+xx）
 }
 
 insert into drink_menu values(null, 'アイスレモンティー', 400, 50, 50)
